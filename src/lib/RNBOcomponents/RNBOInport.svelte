5b68929424fbfe8c31e752d51b6a02b1c4d9e778
tialro <tialro@gmail.com>
2023-10-30 18:07:23 +0100

    Benutze Controls für Inports

--- src/lib/RNBOcomponents/RNBOInport.svelte
+++ src/lib/RNBOcomponents/RNBOInport.svelte
@@ -1,5 +1,5 @@
 <script>
-	import RangeSlider from '../UIcomponents/RangeSlider.svelte';
+	import Controls from '../UIcomponents/Controls.svelte';
 	import { TimeNow, MessageEvent } from '@rnbo/js';
 
 	/** @type {import ('@rnbo/js').MessageInfo} */
@@ -8,22 +8,8 @@
 	export let device;
 	/** @type {number[]} */
 	export let value = [];
-	/** @type {number} */
-	export let min = 0;
-	/** @type {number} */
-	export let max = 1;
 
 	$: device.scheduleEvent(new MessageEvent(TimeNow, inport.tag, value));
 </script>
 
-<!-- only show slider if value is controlled from within the component, otherwise only use the component to change the value -->
-{#if value.length < 2}
-	<div class="RNBOcomponent RNBOsection" {...$$restProps}>
-		<div class="RNBOalign">
-			<div class="RNBOtag">{inport.tag}</div>
-			<div class="RNBOval">{value} / {max}</div>
-		</div>
-		<p>{inport.meta.slice(1, -1)}</p>
-		<RangeSlider name="range-slider" bind:value={value[0]} {min} {max} step={0.01} />
-	</div>
-{/if}
+<Controls />
---
+++ src/lib/UIcomponents/Controls.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+
+	const dispatch = createEventDispatcher();
+
+	const icons = {
+		play: "/play.png",
+		pause: "/pause.png",
+	};
+
+	export let isPlaying: Boolean;
+</script>
+
+<button on:click={() => dispatch("playPause")}>
+	<img src={isPlaying ? icons.pause : icons.play} alt="play" />
+</button>
